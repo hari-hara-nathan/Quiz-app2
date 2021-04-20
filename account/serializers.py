2a99c7c1c068992343e8db5500a7246af625cb81
hari nath <harinath01102000@gmail.com>
2021-04-20 11:19:49 +0530

    first commit

---
+++ account/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import *
+
+class CreateSchool_serializer(serializers.ModelSerializer):
+    name = serializers.CharField(validators=[])
+    class Meta:
+        model = School
+        fields = ('name','district','password')
+
+
+class CreateStudent_serializer(serializers.Serializer):
+    username = serializers.CharField(max_length=16)
+    fullname = serializers.CharField(max_length=30)
+    father_name = serializers.CharField(max_length=30)
+    schoolname= serializers.CharField(max_length=30)
+    password= serializers.CharField(max_length=8)
+
+
+class LoginStudent_serializer(serializers.Serializer):
+    username = serializers.CharField(max_length=16)
+    password= serializers.CharField(max_length=8)
+
+
+class LoginSchool_serializer(serializers.ModelSerializer):
+    name=serializers.CharField(validators=[])
+    class Meta:
+        model = School
+        fields = ('name','password')
